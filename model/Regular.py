69fde1bf4895bceab2f2d9374d2c3a2f7a3ddc52
guanghaoyin <yinguanghao.phd@gmail.com>
2020-08-21 19:58:34 +0800

    Create Regular.py

---
+++ model/Regular.py
@@ -0,0 +1,74 @@
+import torch
+from torch import nn
+
+class Regularization(torch.nn.Module):
+    def __init__(self, model, weight_decay, p=2):
+        '''
+        :param model
+        :param weight_decay
+        :param p
+        '''
+        super(Regularization, self).__init__()
+        if weight_decay <= 0:
+            print("param weight_decay can not <=0")
+            exit(0)
+        self.model = model
+        self.weight_decay = weight_decay
+        self.p = p
+        self.weight_list = self.get_weight(model)
+        self.weight_info(self.weight_list)
+
+    def to(self, device):
+        '''
+        :param device: cude or cpu
+        :return:
+        '''
+        self.device = device
+        super().to(device)
+        return self
+
+    def forward(self, model):
+        self.weight_list = self.get_weight(model)  # 获得最新的权重
+        reg_loss = self.regularization_loss(self.weight_list, self.weight_decay, p=self.p)
+        return reg_loss
+
+    def get_weight(self, model):
+        '''
+        :param model:
+        :return:
+        '''
+        weight_list = []
+        for name, param in model.named_parameters():
+            if 'weight' in name:
+                weight = (name, param)
+                weight_list.append(weight)
+        return weight_list
+
+    def regularization_loss(self, weight_list, weight_decay, p=2):
+        '''
+        :param weight_list:
+        :param p:
+        :param weight_decay:
+        :return:
+        '''
+        # weight_decay=Variable(torch.FloatTensor([weight_decay]).to(self.device),requires_grad=True)
+        # reg_loss=Variable(torch.FloatTensor([0.]).to(self.device),requires_grad=True)
+        # weight_decay=torch.FloatTensor([weight_decay]).to(self.device)
+        # reg_loss=torch.FloatTensor([0.]).to(self.device)
+        reg_loss = 0
+        for name, w in weight_list:
+            l2_reg = torch.norm(w, p=p)
+            reg_loss = reg_loss + l2_reg
+
+        reg_loss = weight_decay * reg_loss
+        return reg_loss
+
+    def weight_info(self, weight_list):
+        '''
+        :param weight_list:
+        :return:
+        '''
+        print("---------------regularization weight---------------")
+        for name, w in weight_list:
+            print(name)
+        print("---------------------------------------------------")
